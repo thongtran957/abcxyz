83c77ba44ccb33d7f9453ce3546cbdf72b90b190
thongtran957 <baibuvietnam@gmail.com>
2020-03-13 15:35:43 +0700

    typing

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.admin.master')
 @section('content')
 	<h1>dashboard</h1>
 @endsection
--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -1,9 +1,9 @@
-@include('layouts.header')
+@include('layouts.admin.header')
     <div class="container-fluid page-body-wrapper">
-      @include('layouts.sidebar')
+      @include('layouts.admin.sidebar')
       <div class="main-panel">
         <div class="content-wrapper">
           @yield('content')
         </div>
-        @include('layouts.footer')
+        @include('layouts.admin.footer')
   
